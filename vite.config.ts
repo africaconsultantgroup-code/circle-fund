4edce5d454b2472a55402b23fd830ead95d385a7
africaconsultantgroup-code <africa.consultant.group@gmail.com>
2026-06-02 08:30:16 +0000

    FixTailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwind from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), tailwind()],
 });
